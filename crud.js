49092ce4c5ad7d22fe2627be9b63073d55d9425a
Arman Ortega <ortegaaa@ph.ibm.com>
2016-07-22 15:54:15 +0800

    Update crud.js

--- crud.js
+++ crud.js
@@ -12,7 +12,7 @@ var async = require('async'),
 var createDatabase = function(callback) {
   console.log("Creating database '" + dbname  + "'");
   cloudant.db.create(dbname, function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     db = cloudant.db.use(dbname);
     callback(err, data);
@@ -25,7 +25,7 @@ var createDocument = function(callback) {
   console.log("Creating document 'mydoc'");
   // we are specifying the id of the document so we can update and delete it later
   db.insert({ _id: "mydoc", a:1, b: "two"}, function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     callback(err, data);
   });
@@ -36,7 +36,7 @@ var readDocument = function(callback) {
   console.log(" ");
   console.log("Reading document 'mydoc'");
   db.get("mydoc", function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     // keep a copy of the doc so we know its revision token
     doc = data;
@@ -47,12 +47,16 @@ var readDocument = function(callback) {
 // update a document
 var updateDocument = function(callback) {
   console.log(" ");
-  console.log("Updating document 'mydoc'");
-  console.log(doc);
+  console.log("Updating document 'mydoc':");
+  console.log("BEFORE data: ", doc);
   // make a change to the document, using the copy we kept from reading it back
   doc.c = true;
+  doc.d = {
+    'foo': 'bar'
+  }
+  console.log("AFTER data: ", doc);
   db.insert(doc, function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     // keep the revision of the update so we can delete it
     doc._rev = data.rev;
@@ -62,23 +66,29 @@ var updateDocument = function(callback) {
 
 // deleting a document
 var deleteDocument = function(callback) {
-  console.log("Deleting document 'mydoc'");
+  console.log(" ");
+  console.log("Deleting document 'mydoc' :");
+  console.log(doc);
   // supply the id and revision to be deleted
   db.destroy(doc._id, doc._rev, function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     callback(err, data);
   });
 };
 
 // deleting the database document
-var deleteDatabase = function(callback) {
+/*var deleteDatabase = function(callback) {
   console.log("Creating database '" + dbname  + "'");
   cloudant.db.destroy(dbname, function(err, data) {
-    console.log("Error:", err);
+    if(err) console.log("Error:", err);
     console.log("Data:", data);
     callback(err, data);
   });
+};*/
+var deleteDatabase = function(callback) {
+  console.log("Deleting database '" + dbname  + "'");
+  callback(err, data);
 };
-
-async.series([createDatabase, createDocument, readDocument, updateDocument, deleteDocument, deleteDatabase ]);
+// async.series([createDatabase, createDocument, readDocument, updateDocument, deleteDocument, deleteDatabase ]);
+async.series([createDocument, readDocument, updateDocument, deleteDocument]);
